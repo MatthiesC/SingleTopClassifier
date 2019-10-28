3046505e5e8c25a399d25aff4dfbccff40e56524
Christopher Matthies <christopher.matthies@desy.de>
2019-10-28 16:41:25 +0100

    Input variables are now normalized according to parameters set in InputDefintion

--- config/InputDefinition.py
+++ config/InputDefinition.py
@@ -2,119 +2,126 @@
 # Specify how many HOTVR and AK4 jets you want to use!
 
 import os, sys
+import numpy as np
+
+# scheme: [variable name, estimated lower limit, estimated upper limit, boolean (use it for DNN?)]
+# limit values are used for normalizing the input vector entries to interval [0,1]. If there is a case where an event has values outside the given limits, then there will be an entry with a value outside of the interval [0,1] but this shoud not matter that much. We just need this to numerically stabilize the DNN training.
 
 template_event = [
-    ["n_pv",False],
-    ["met_px",False],
-    ["met_py",False],
-    ["met_pt",True],
-    ["met_phi",True],
-    ["ht_had",True],
-    ["ht_lep",True],
-    ["st",True],
-    ["n_hotvr",True],
-    ["n_jets",True],
-    ["n_btags",True]
+    ["n_pv",0,100,False],
+    ["met_px",-2000,2000,False],
+    ["met_py",-2000,2000,False],
+    ["met_pt",0,2000,True],
+    ["met_phi",-4,4,True],
+    ["ht_had",0,7000,True],
+    ["ht_lep",0,2000,True],
+    ["st",0,7000,True],
+    ["n_hotvr",0,10,True],
+    ["n_jets",0,20,True],
+    ["n_btags",0,10,True]
 ]
 template_hotvr = [
-    ["toptagged",True],
-    ["px",False],
-    ["py",False],
-    ["pz",False],
-    ["e",False],
-    ["pt",True],
-    ["eta",True],
-    ["phi",True],
-    ["v4mass",True],
-    ["area",False],
-    ["nsubjets",True],
-    ["sub1_px",False],
-    ["sub1_py",False],
-    ["sub1_pz",False],
-    ["sub1_e",False],
-    ["sub1_pt",False],
-    ["sub1_eta",False],
-    ["sub1_phi",False],
-    ["sub1_v4mass",False],
-    ["sub1_area",False],
-    ["sub2_px",False],
-    ["sub2_py",False],
-    ["sub2_pz",False],
-    ["sub2_e",False],
-    ["sub2_pt",False],
-    ["sub2_eta",False],
-    ["sub2_phi",False],
-    ["sub2_v4mass",False],
-    ["sub2_area",False],
-    ["sub3_px",False],
-    ["sub3_py",False],
-    ["sub3_pz",False],
-    ["sub3_e",False],
-    ["sub3_pt",False],
-    ["sub3_eta",False],
-    ["sub3_phi",False],
-    ["sub3_v4mass",False],
-    ["sub3_area",False],
-    ["fpt",True],
-    ["mpair",True],
-    ["tau1",True],
-    ["tau2",True],
-    ["tau3",True],
-    ["tau21",True],
-    ["tau32",True]
+    ["toptagged",0,1,True],
+    ["px",-2000,2000,False],
+    ["py",-2000,2000,False],
+    ["pz",-5000,5000,False],
+    ["e",0,7000,False],
+    ["pt",0,2000,True],
+    ["eta",-5,5,True],
+    ["phi",-4,4,True],
+    ["v4mass",0,500,True],
+    ["area",0,20,False],
+    ["nsubjets",0,10,True],
+    ["sub1_px",-2000,2000,False],
+    ["sub1_py",-2000,2000,False],
+    ["sub1_pz",-2000,2000,False],
+    ["sub1_e",0,2000,False],
+    ["sub1_pt",0,1000,False],
+    ["sub1_eta",-5,5,False],
+    ["sub1_phi",-4,4,False],
+    ["sub1_v4mass",0,100,False],
+    ["sub1_area",0,15,False],
+    ["sub2_px",-2000,2000,False],
+    ["sub2_py",-2000,2000,False],
+    ["sub2_pz",-2000,2000,False],
+    ["sub2_e",0,2000,False],
+    ["sub2_pt",0,1000,False],
+    ["sub2_eta",-5,5,False],
+    ["sub2_phi",-4,4,False],
+    ["sub2_v4mass",0,100,False],
+    ["sub2_area",0,15,False],
+    ["sub3_px",-2000,2000,False],
+    ["sub3_py",-2000,2000,False],
+    ["sub3_pz",-2000,2000,False],
+    ["sub3_e",0,2000,False],
+    ["sub3_pt",0,1000,False],
+    ["sub3_eta",-5,5,False],
+    ["sub3_phi",-4,4,False],
+    ["sub3_v4mass",0,100,False],
+    ["sub3_area",0,15,False],
+    ["fpt",0,1,True],
+    ["mpair",0,300,True],
+    ["tau1",0,1,True],
+    ["tau2",0,1,True],
+    ["tau3",0,1,True],
+    ["tau21",0,1,True],
+    ["tau32",0,1,True]
 ]
 template_jet = [
-    ["btagged",False],
-    ["DeepJet",True],
-    ["px",False],
-    ["py",False],
-    ["pz",False],
-    ["e",False],
-    ["pt",True],
-    ["eta",True],
-    ["phi",True],
-    ["v4mass",True],
-    ["area",False]
+    ["btagged",0,1,False],
+    ["DeepJet",0,1,True],
+    ["px",-2000,2000,False],
+    ["py",-2000,2000,False],
+    ["pz",-2000,2000,False],
+    ["e",0,7000,False],
+    ["pt",0,3000,True],
+    ["eta",-5,5,True],
+    ["phi",-4,4,True],
+    ["v4mass",0,300,True],
+    ["area",0,2,False]
 ]
 template_lepton = [
-    ["px",False],
-    ["py",False],
-    ["pz",False],
-    ["e",True],
-    ["pt",True],
-    ["eta",True],
-    ["phi",True],
-    ["v4mass",False],
-    ["reliso",True],
-    ["charge",True],
-    ["dr_jet",True],
-    ["dphi_jet",True],
-    ["ptrel_jet",True]
+    ["px",-2000,2000,False],
+    ["py",-2000,2000,False],
+    ["pz",-2000,2000,False],
+    ["e",0,7000,True],
+    ["pt",0,2000,True],
+    ["eta",-5,5,True],
+    ["phi",-4,4,True],
+    ["v4mass",-1,1,False], # measured in GeV, muon mass ca. 106 MeV, negative values possible
+    ["reliso",0,0.2,True],
+    ["charge",-1,1,True],
+    ["dr_jet",0,6,True],
+    ["dphi_jet",0,4,True],
+    ["ptrel_jet",0,7000,True]
 ]
 
 
 def compileInputList(n_hotvr, n_jets):
+
+    """Returns an array of all DNN input variables together with their normalization parameters."""
+
     print "Number of HOTVR jets considered for input vector:  "+str(n_hotvr)
     print "Number of AK4   jets considered for input vector:  "+str(n_jets)
     n_hotvr = int(n_hotvr)
     n_jets = int(n_jets)
     inputList = []
     for var in template_event:
-        if var[1] == True:
-            inputList.append("DNN__"+var[0])
+        if var[-1] == True:
+            inputList.append(["DNN__"+var[0], float(var[1]), 1/float(var[2]-var[1])])
     for i in range(n_hotvr):
         for var in template_hotvr:
-            if var[1] == True:
-                inputList.append("DNN__hotvr"+str(i+1)+"_"+var[0])
+            if var[-1] == True:
+                inputList.append(["DNN__hotvr"+str(i+1)+"_"+var[0], float(var[1]), 1/float(var[2]-var[1])])
     for i in range(n_jets):
         for var in template_jet:
-            if var[1] == True:
-                inputList.append("DNN__jet"+str(i+1)+"_"+var[0])
+            if var[-1] == True:
+                inputList.append(["DNN__jet"+str(i+1)+"_"+var[0], float(var[1]), 1/float(var[2]-var[1])])
     for var in template_lepton:
-        if var[1] == True:
-            inputList.append("DNN__lepton_"+var[0])
+        if var[-1] == True:
+            inputList.append(["DNN__lepton_"+var[0], float(var[1]), 1/float(var[2]-var[1])])
     print "Length of input vector:                            "+str(len(inputList))
-    return inputList
+    return np.array(inputList)
 
 
 def main(n_hotvr, n_jets):
--- train.py
+++ train.py
@@ -17,11 +17,21 @@ def get_InputVariableNames(n_hotvr=2, n_jets=4):
     return inputList
 
 
-def read_InputVariables(rootFileName, inputList, treeName='AnalysisTree'):
+def read_InputVariables(rootFileName, inputArray, treeName='AnalysisTree'):
     """Reads all DNN input variables from a given ROOT file and translates the input into DataFrame format."""
-    numpyArray = root2array(rootFileName, treename=treeName, branches=inputList)
+    inputNames = inputArray[:,0] # get only the string names
+    numpyArray = root2array(rootFileName, treename=treeName, branches=inputNames)
     dataFrame = pandas.DataFrame(numpyArray)
     return dataFrame
+
+
+def normalize_InputVectorEntries(numpyInput, inputParameters):
+    """Takes the input vector in numpy format and returns a new array with normalized input variables based on normalization parameters."""
+    normalizedInput = numpyInput
+    for entry in normalizedInput:
+        for i in range(len(inputParameters)):
+            entry[i] = (float(entry[i])-float(inputParameters[i,1]))*float(inputParameters[i,2])
+    return normalizedInput
 
 
 def create_Path(path):
@@ -32,10 +42,10 @@ def create_Path(path):
         print "Created path '%s'" % (path)
 
 
-def define_KerasModel(inputList):
+def define_KerasModel(inputArray):
     """Defines the DNN model."""
     model = Sequential()
-    model.add(Dense(512, input_dim=len(inputList), activation='relu'))
+    model.add(Dense(512, input_dim=len(inputArray), activation='relu'))
     model.add(Dense(512, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
     return model
@@ -50,7 +60,13 @@ def main():
     dataFrame = read_InputVariables(fileNamePrefix_MC+dict_Classes['QCD']['File'], inputVariables)
     create_Path('workdir')
     np.save('workdir/QCD.npy', dataFrame)
-    print np.load('workdir/QCD.npy')
+    loaded_input = np.load('workdir/QCD.npy')
+    print "Input vector:"
+    print loaded_input
+    norm_input = normalize_InputVectorEntries(loaded_input, inputVariables)
+    np.save('workdir/QCD_norm.npy', norm_input)
+    print "Normalized input vector:"
+    print norm_input
 
 
 if __name__=="__main__":
