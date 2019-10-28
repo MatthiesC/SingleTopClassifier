2180c41459cbb442c143dffb320e2a96f0a0b178
Christopher Matthies <christopher.matthies@desy.de>
2019-10-29 00:04:48 +0100

    Add bash script to scp ntuples from dust to Maxwell

---
+++ ReceiveNtuples.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+declare -a arr=("ST_tW_signal" "ST_tW_other" "ST_non-tW_t-Ch" "ST_non-tW_s-Ch" "TTbar" "WJets" "DYJets" "Diboson" "QCD")
+
+for i in "${arr[@]}"
+do
+    scp matthies@naf-cms:/nfs/dust/cms/user/matthies/HighPtSingleTop/102X_v1/MainSelection/2016/Muon/uhh2.AnalysisModuleRunner.MC.${i}.root ./samples/.
+done
