2e90f388b7ac669336c41510883e34be46f2f04d
Christopher Matthies <christopher.matthies@desy.de>
2019-10-28 22:28:40 +0100

    Add single top t-channel and s-channel to the list of samples

--- config/SampleClasses.py
+++ config/SampleClasses.py
@@ -3,27 +3,35 @@ fileNamePrefix_DATA = "samples/uhh2.AnalysisModuleRunner.DATA."
 
 dict_Classes = {
     "tW_signal":{
-        "Use":False,
+        "Use":True,
         "File":"ST_tW_signal.root"
     },
     "tW_other":{
-        "Use":False,
+        "Use":True,
         "File":"ST_tW_other.root"
     },
+    "tChannel":{
+        "Use":True,
+        "File":"ST_non-tW_t-Ch.root"
+    },
+    "sChannel":{
+        "Use":True,
+        "File":"ST_non-tW_s-Ch.root"
+    },
     "TTbar":{
-        "Use":False,
+        "Use":True,
         "File":"TTbar.root"
     },
     "WJets":{
-        "Use":False,
+        "Use":True,
         "File":"WJets.root"
     },
     "DYJets":{
-        "Use":False,
+        "Use":True,
         "File":"DYJets.root"
     },
     "Diboson":{
-        "Use":False,
+        "Use":True,
         "File":"Diboson.root"
     },
     "QCD":{
