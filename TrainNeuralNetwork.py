db71e503afa7ee1e2b9cb58b640914c2f2e4d824
Christopher Matthies <christopher.matthies@desy.de>
2019-10-29 13:32:41 +0100

    Add function to split samples into train/test/valdiation data sets

--- TrainNeuralNetwork.py
+++ TrainNeuralNetwork.py
@@ -1,5 +1,7 @@
 from config.InputDefinition import compileInputList
+from config.SampleClasses import *
 
+import sys
 import numpy as np
 
 from keras.models import Sequential
@@ -10,6 +12,54 @@ from keras.layers import Dense
 
 inputVariableNames = (compileInputList())[:,0]
 #print inputVariableNames
+
+
+def load_Numpy(processName, inputSuffix='_norm', workdirName='workdir'):
+
+    """Loads numpy file from work directory. Needs process name (TTbar, QCD, etc.) and file name suffix (default = '_norm')."""
+
+    fileName = './'+workdirName+'/'+processName+inputSuffix+'.npy'
+    return np.load(fileName)
+
+
+def split_TrainTestValidation(processName, percentTrain, percentTest, percentValidation, inputSuffix='_norm', workdirName='workdir'):
+
+    """Splits a given numpy sample into training, test, and validation numpy files. Returns list of file names of train, test, and validation numpy files."""
+
+    if percentTrain+percentTest+percentValidation > 1: sys.exit("Sum of percentages for training, test, and validation samples is greater than 1. Exit.")
+
+    print "Load numpy file for process:",processName
+
+    loaded_numpy = load_Numpy(processName, inputSuffix, workdirName)
+    cardinality = len(loaded_numpy)
+
+    print "Cardinality:",cardinality
+
+    absoluteTrain = int(cardinality*percentTrain)
+    absoluteTest = int(cardinality*percentTest)
+    absoluteValidation = int(cardinality*percentValidation)
+    
+    print "Will split set into train/test/valdiation samples of sizes:",absoluteTrain,absoluteTest,absoluteValidation
+
+    numpyTrain = loaded_numpy[0:absoluteTrain]
+    numpyTest = loaded_numpy[absoluteTrain:absoluteTrain+absoluteTest]
+    numpyValidation = loaded_numpy[absoluteTrain+absoluteTest:absoluteTrain+absoluteTest+absoluteValidation]
+    
+    fileNameTrain = './'+workdirName+'/'+processName+inputSuffix+'_train.npy'
+    fileNameTest = './'+workdirName+'/'+processName+inputSuffix+'_test.npy'
+    fileNameValidation = './'+workdirName+'/'+processName+inputSuffix+'_validation.npy'
+
+    fileNames = [fileNameTrain, fileNameTest, fileNameValidation]
+
+    print "Saving numpy files..."
+
+    np.save(fileNameTrain, numpyTrain)
+    np.save(fileNameTest, numpyTest)
+    np.save(fileNameValidation, numpyValidation)
+
+    print "Done saving."
+
+    return fileNames
 
 
 def define_Model():
@@ -35,4 +85,4 @@ def train_NN():
 
 if __name__ == '__main__':
 
-    train_NN()
+    split_TrainTestValidation('QCD', 0.6, 0.2, 0.2)
