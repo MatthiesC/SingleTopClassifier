92536f3aa567f4b6970e76a406b0a010a03be931
Christopher Matthies <Christopher.Matthies@desy.de>
2019-11-15 13:42:08 +0100

    Option to switch between cp/scp when receiving Ntuples

--- ReceiveNtuples.sh
+++ ReceiveNtuples.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+echo "Use scp? [y/n]:"
+
+read answer_scp
+
 mkdir -p ./samples
 
 declare -a arr=("ST_tW_signal" "ST_tW_other" "ST_non-tW_t-Ch" "ST_non-tW_s-Ch" "TTbar" "WJets" "DYJets" "Diboson" "QCD")
 
 for i in "${arr[@]}"
 do
-    scp matthies@naf-cms:/nfs/dust/cms/user/matthies/HighPtSingleTop/102X_v1/MainSelection/2016/Muon/uhh2.AnalysisModuleRunner.MC.${i}.root ./samples/.
+    if [ $answer_scp == "y" ]; then
+	scp matthies@naf-cms:/nfs/dust/cms/user/matthies/HighPtSingleTop/102X_v1/MainSelection/2016/Muon/uhh2.AnalysisModuleRunner.MC.${i}.root ./samples/.
+    else
+	cp /nfs/dust/cms/user/matthies/HighPtSingleTop/102X_v1/MainSelection/2016/Muon/uhh2.AnalysisModuleRunner.MC.${i}.root ./samples/.
+    fi
 done
