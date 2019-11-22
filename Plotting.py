7302681dd4b8f40de287017e0f4e99977b2105ca
Christopher Matthies <Christopher.Matthies@desy.de>
2019-11-22 15:59:27 +0100

    Make nicer loss/accuracy plots

--- Plotting.py
+++ Plotting.py
@@ -5,81 +5,51 @@ import matplotlib.pyplot as plt
 import sys
 import os
 import pickle
+import numpy as np
 
 
-def plot_CustomLoss(dnnTag):
+def insert_InfoBox(axisObject, customHist, kerasHist, type='loss', n_last_epochs=20):
 
-    print('Plotting loss history. - Custom version')
-
-    model_history = None
-    pickleFile = './outputs/'+dnnTag+'/model_customHistory.pkl'
-    with open(pickleFile, 'rb') as f:
-        model_history = pickle.load(f)
-
-    plt.clf()
-    fig = plt.figure()
-    plt.grid()
-    x = (range(len(model_history['train_loss'])+1))[1:]
-    plt.plot(x, model_history['train_loss'], label='Training set')
-    #plt.plot(x, model_history['valid_loss'], label='Validation set')
-    plt.legend(loc='upper right')
-    plt.ylabel('Loss (categorical crossentropy)')
-    plt.xlabel('Number of training epochs')
-    saveFile = './outputs/'+dnnTag+'/plots/loss_custom.pdf'
-    fig.savefig(saveFile)
-    plt.close()
+    train_x = np.array(customHist['train_'+str(type)][-n_last_epochs:])
+    valid_x = np.array(kerasHist['val_'+str(type)][-n_last_epochs:])
+    textstr = '\n'.join((
+        r'After epoch '+str(len(customHist['train_'+str(type)]))+':',
+        r'Training set: %.3f %%' % (100*customHist['train_'+str(type)][-1], ),
+        r'Validation set: %.3f %%' % (100*kerasHist['val_'+str(type)][-1], ),
+        r'Last '+str(n_last_epochs)+' epochs:',
+        r'Training set: $%.3f \pm %.3f$ %%' % (100*train_x.mean(), 100*train_x.std(), ),
+        r'Validation set: $%.3f \pm %.3f$ %%' % (100*valid_x.mean(), 100*valid_x.std(), )))
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+    axisObject.text(0.5, 0.5, textstr, transform=axisObject.transAxes, bbox=props)
 
 
 def plot_Loss(dnnTag):
 
-    print('Plotting loss history. - Buggy Keras version')
+    print('Plotting loss history.')
 
-    model_history = None
     pickleFile = './outputs/'+dnnTag+'/model_history.pkl'
     with open(pickleFile, 'rb') as f:
         model_history = pickle.load(f)
 
-    plt.clf()
-    fig = plt.figure()
-    plt.grid()
-    x = (range(len(model_history['loss'])+1))[1:]
-    plt.plot(x, model_history['loss'], label='Training set')
-    plt.plot(x, model_history['val_loss'], label='Validation set')
-    plt.legend(loc='upper right')
-    plt.ylabel('Loss (categorical crossentropy)')
-    plt.xlabel('Number of training epochs')
-    saveFile = './outputs/'+dnnTag+'/plots/loss.pdf'
-    fig.savefig(saveFile)
-    plt.close()
-
-
-def plot_LossBoth(dnnTag):
-
-    print('Plotting loss history. - Both versions: Keras and custom')
-
-    model_history = None
-    pickleFile = './outputs/'+dnnTag+'/model_history.pkl'
-    with open(pickleFile, 'rb') as f:
-        model_history = pickle.load(f)
-
-    model_customHistory = None
     pickleFileCustom = './outputs/'+dnnTag+'/model_customHistory.pkl'
     with open(pickleFileCustom, 'rb') as f:
         model_customHistory = pickle.load(f)
 
     plt.clf()
-    fig = plt.figure()
+    fig, ax = plt.subplots()
     plt.grid()
-    x = (range(len(model_history['loss'])+1))[1:]
-    plt.plot(x, model_history['loss'], label='Training set (vanilla Keras history)')
-    plt.plot(x, model_customHistory['train_loss'], label='Training set (custom history callback)')
-    plt.plot(x, model_history['val_loss'], label='Validation set (vanilla Keras history)')
-    #plt.plot(x, model_customHistory['valid_loss'], label='Validation set (custom history callback)', linestyle='--')
+    x = (range(len(model_customHistory['train_loss'])+1))[1:]
+    plt.plot(x, model_customHistory['train_loss'], label='Training set')
+    plt.plot(x, model_history['val_loss'], label='Validation set', linestyle='--')
     plt.legend(loc='upper right')
-    plt.ylim([0.00, 0.35])
+    plt.ylim([0.00, 0.25])
     plt.ylabel('Loss (categorical crossentropy)')
     plt.xlabel('Number of training epochs')
-    saveFile = './outputs/'+dnnTag+'/plots/loss_both.pdf'
+    plt.title('Tag: '+dnnTag)
+
+    insert_InfoBox(ax, model_customHistory, model_history, 'loss')
+
+    saveFile = './outputs/'+dnnTag+'/plots/loss.pdf'
     fig.savefig(saveFile)
     plt.close()
 
@@ -87,22 +57,31 @@ def plot_LossBoth(dnnTag):
 def plot_Metrics(dnnTag):
 
     print('Plotting metrics history.')
-    
-    model_history = None
-    with open('./outputs/'+dnnTag+'/model_history.pkl', 'rb') as f:
+
+    pickleFile = './outputs/'+dnnTag+'/model_history.pkl'
+    with open(pickleFile, 'rb') as f:
         model_history = pickle.load(f)
 
+    pickleFileCustom = './outputs/'+dnnTag+'/model_customHistory.pkl'
+    with open(pickleFileCustom, 'rb') as f:
+        model_customHistory = pickle.load(f)
+    
     plt.clf()
-    fig = plt.figure()
+    fig, ax = plt.subplots()
     plt.grid()
-    x = (range(len(model_history['categorical_accuracy'])+1))[1:]
-    plt.plot(x, model_history['categorical_accuracy'], label='Training set')
-    plt.plot(x, model_history['val_categorical_accuracy'], label='Validation set')
+    x = (range(len(model_customHistory['train_categorical_accuracy'])+1))[1:]
+    plt.plot(x, model_customHistory['train_categorical_accuracy'], label='Training set')
+    plt.plot(x, model_history['val_categorical_accuracy'], label='Validation set', linestyle='--')
     plt.legend(loc='lower right')
     plt.ylim([0.00, 1.00])
     plt.ylabel('Categorical accuracy')
     plt.xlabel('Number of training epochs')
-    fig.savefig('./outputs/'+dnnTag+'/plots/accuracy.pdf')
+    plt.title('Tag: '+dnnTag)
+    
+    insert_InfoBox(ax, model_customHistory, model_history, 'categorical_accuracy')
+
+    saveFile = './outputs/'+dnnTag+'/plots/accuracy.pdf'
+    fig.savefig(saveFile)
     plt.close()
 
 
@@ -111,7 +90,5 @@ if __name__ == '__main__':
     dnnTag = sys.argv[1] # dnn_yy-mm-dd-hh-mm-ss
     os.makedirs('outputs/'+dnnTag+'/plots', exist_ok=True)
 
-    plot_CustomLoss(dnnTag)
     plot_Loss(dnnTag)
-    plot_LossBoth(dnnTag)
     plot_Metrics(dnnTag)
