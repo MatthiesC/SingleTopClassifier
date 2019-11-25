6d0160497b015576153d8f4711514872d5ece76f
Christopher Matthies <Christopher.Matthies@desy.de>
2019-11-25 14:22:22 +0100

    Add colors to classes

--- config/SampleClasses.py
+++ config/SampleClasses.py
@@ -4,46 +4,57 @@ fileNamePrefix_DATA = "samples/uhh2.AnalysisModuleRunner.DATA."
 dict_Classes = {
     "tW_signal":{
         "Use":True,
-        "File":"ST_tW_signal.root"
+        "File":"ST_tW_signal.root",
+        "color": "blue"
     },
     "tW_other":{
         "Use":True,
-        "File":"ST_tW_other.root"
+        "File":"ST_tW_other.root",
+        "color": "cyan"
     },
     "tW_bkg_TopToHadAndWToTau":{
         "Use":True,
-        "File":"ST_tW_bkg_TopToHadAndWToTau.root"
+        "File":"ST_tW_bkg_TopToHadAndWToTau.root",
+        "color": "deepskyblue"
     },
     "tW_bkg_Else":{
         "Use":True,
-        "File":"ST_tW_bkg_Else.root"
+        "File":"ST_tW_bkg_Else.root",
+        "color": "cyan"
     },
     "tChannel":{
         "Use":True,
-        "File":"ST_non-tW_t-Ch.root"
+        "File":"ST_non-tW_t-Ch.root",
+        "color": "orange"
     },
     "sChannel":{
         "Use":True,
-        "File":"ST_non-tW_s-Ch.root"
+        "File":"ST_non-tW_s-Ch.root",
+        "color": "brown"
     },
     "TTbar":{
         "Use":True,
-        "File":"TTbar.root"
+        "File":"TTbar.root",
+        "color": "red"
     },
     "WJets":{
         "Use":True,
-        "File":"WJets.root"
+        "File":"WJets.root",
+        "color": "green"
     },
     "DYJets":{
         "Use":True,
-        "File":"DYJets.root"
+        "File":"DYJets.root",
+        "color": "yellow"
     },
     "Diboson":{
         "Use":True,
-        "File":"Diboson.root"
+        "File":"Diboson.root",
+        "color": "magenta"
     },
     "QCD":{
         "Use":True,
-        "File":"QCD.root"
+        "File":"QCD.root",
+        "color": "gray"
     }
 }
