a69ce55818a5ad91e5a090e8b15975bbbea14f75
Christopher Matthies <christopher.matthies@desy.de>
2020-01-23 00:33:07 +0100

    Fixed bugs in submission configuration

--- sbatch_job.sh
+++ sbatch_job.sh
@@ -3,15 +3,15 @@
 
 ### ENTER SLURM JOB COMMANDS HERE:
 
-#SBATCH --partion=cms-uhh,cms,all-gpu
+#SBATCH --partition=cms-uhh,cms,allgpu
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --mail-type ALL
 #SBATCH --mail-user christopher.matthies@desy.de
 #SBATCH --constraint=GPU
-#SBATCH --workdir /beegfs/desy/user/matthies/WorkingArea/SingleTopClassifier/
-#SBATCH -o ./output/job_outputs/slurm.%N.%j.out
-#SBATCH -e ./output/job_outputs/slurm.%N.%j.err
+#SBATCH --chdir /beegfs/desy/user/matthies/WorkingArea/SingleTopClassifier/
+#SBATCH -o ./outputs/job_outputs/slurm.%N.%j.out
+#SBATCH -e ./outputs/job_outputs/slurm.%N.%j.err
 
 
 ### COMMANDS WHICH THE JOB CONSISTS OF:
